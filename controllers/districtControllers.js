5afee0de3948f0128bb509b57cf8dc5e0dacb423
rahul14kumar <rahul.kumar.it25@heritageit.edu.in>
2024-04-11 15:11:15 +0530

    district done

--- controllers/districtControllers.js
+++ controllers/districtControllers.js
@@ -1,4 +1,5 @@
 const client=require('../database/db');
+const { get } = require('../routes/userRoutes');
 
 const createDistrictTable = async (req, res) => {
     try{
@@ -18,6 +19,53 @@ const createDistrictTable = async (req, res) => {
     }
 }
 
+// admin create district
+const createDistrict=async(req,res)=>{
+    try{
+        const {role}=req.user;
+        const {d_name}=req.body;
+
+        if(role!=="admin"){
+            res.status(400);
+            throw new Error("Not authorized to create district");
+        }else{
+            const create=await client.query(
+                `INSERT INTO districts(d_name) VALUES($1)`,[d_name]
+            );
+            console.log("create district",create);
+            res.status(200).json({message:"District created"}); 
+        }
+
+    }catch(error){
+        console.log("get error from create district",error);
+        res.status(500).json({error:error});
+    }
+}
+
+// admin can get all district
+const getAllDistrict=async(req,res)=>{
+    try{
+        const {role}=req.user;
+
+        if(role!=="admin"){
+            res.status(400);
+            throw new Error("Not authorized to get all district");
+        }else{
+            const get=await client.query(
+                `SELECT * FROM districts`
+            );
+            console.log("get all district",get);
+            res.status(200).json({districts:get.rows});
+        }
+
+    }catch(error){
+        console.log("get error from get all district",error);
+        res.status(500).json({error:error});
+    }
+}
+
 module.exports={
-    createDistrictTable
+    createDistrictTable,
+    getAllDistrict,
+    createDistrict
 }
--- routes/districtRoutes.js
+++ routes/districtRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { createDistrictTable } = require("../controllers/districtControllers");
+const { createDistrictTable, getAllDistrict, createDistrict } = require("../controllers/districtControllers");
+const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.get('/createTable', createDistrictTable);
+router.get('/getAlldistricts',protect, getAllDistrict);
+
+router.post('/createDistrict',protect,createDistrict)
 
 module.exports=router;
